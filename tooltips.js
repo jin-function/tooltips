ea114b90900c8d49e23faf35335914486662c270
jin-function <45027934+jin-function@users.noreply.github.com>
2019-03-13 17:07:12 +0900

    버그 수정.
    외부에 margin 사용시 잘못된 위치 교정.
    .tooltip-wrapper 미 사용시 잘못된 위치 교정.

--- tooltips.js
+++ tooltips.js
@@ -5,22 +5,30 @@ function Tooltip(e){
     let Jthis;     // target
     let tooltip;   // tooltip tag
     let Timer; 
-    let data;
     let ready = false;
+
+    let seting = {
+        parent:  {top: 0, left:0, width:0, height:0},
+        selecter:{top: 0, left:0, width:0, height:0},
+        tooltip: {top: 0, left:0, width:0, height:0},
+        body:    {top: 0, left:0, width:0, height:0},
+        title:   'Title is not found',
+        position:'under'
+    };
 
     /* check data */
     let wrap;
     let wrapChecker;
 
     el.mouseenter( function(e){
-        data = initData(e);
-        if(ready){ // open tips
+        initData(e);
+        if(ready){ 
             clearTimeout(Timer);
-            openTip(data);
+            openTip();
         } else {
            Timer = setTimeout(function(){
                 ready = true;
-                openTip(data);
+                openTip();
             }, 500);            
         }
     })
@@ -38,113 +46,122 @@ function Tooltip(e){
     function initData(e){
         Jthis = $(e.target);
         Selecter_Checker();  //올바른 selecter인지 체크하고 랩퍼로 사용할 부모 클래스를 할당한다.
+        Jthis.attr('title', '');
 
-        let parentTop       = wrap.offset().top;
-        let parentLeft      = wrap.offset().left;
-        let parentWidth     = wrap.outerWidth();
-        let parentHeight    = wrap.outerHeight();
-        let selecterWidth   = Jthis.outerWidth(true);
-        let selecterHeight  = Jthis.outerHeight(true);
-        let selecterTop     = Jthis.position().top;
-        let selecterLeft    = Jthis.position().left;
+        /* parent setting */
+        seting.parent.top    = wrapChecker ? wrap.offset().top:0;
+        seting.parent.left   = wrap.offset().left;
+        seting.parent.width  = wrap.outerWidth();
+        seting.parent.height = wrap.outerHeight();
 
-        var set = {
-            "PTop":     parentTop,
-            "PLeft":    parentLeft,
-            "PWidth":   parentWidth,
-            "PHeight":  parentHeight,
-            "width":    selecterWidth,
-            "height":   selecterHeight,
-            "Top":      selecterTop,
-            "Left":     selecterLeft, 
-            "Title":    title
-        };
+        /* selecter setting */
+        seting.selecter.top    = Jthis.position().top;
+        seting.selecter.left   = Jthis.position().left;
+        seting.selecter.width  = Jthis.outerWidth(true);
+        seting.selecter.height = Jthis.outerHeight(true);
 
         return {
             parent: wrap,
-            el:Jthis,
-            set: set
+            el:Jthis
         }
     }
 
-    function openTip(data){
+    function openTip(){
         let html = '<div class="tooltip-box"><p class="title"></p></div>';
-        Jthis.attr('title', '');
-        data.parent.append(html);
+        wrap.append(html);
         tooltip = $('.tooltip-box');
-        tooltip.find('.title').text(data.set['Title']);  
-        SetPosition(data.set);   
+        tooltip.find('.title').text(seting.title);  
+        SetPosition();   
     }
 
     function closeTip(){
-        Jthis.attr('title', data.set['Title']);
         tooltip? tooltip.css('opacity',0):0;
-        data.parent.find('.tooltip-box').remove();
+        wrap.find('.tooltip-box').remove();
+        Jthis.attr('title', seting.title);
     }
 
     function Selecter_Checker(){
+        //tooltip-wrapper class check.
         wrap = Jthis.parents('.tooltip-wrapper');
-        wrapChecker = wrap.length == 0 ? false:true; //wrap 클래스 선언여부 체크.
-        /* 하위 태그가 선택됬는지 체크 */
-        let check = Jthis.data('toggle');
-        // tooltip 하위태그 선택되는 이슈. 셀렉터 재할당.
-        typeof check == 'undefined' ? Jthis = Jthis.parents('[data-toggle="tooltip"]'):true; 
-        wrap  = wrapChecker ? wrap:Jthis.parent();    
-        title = Jthis.attr('title');
-        title = typeof title == 'undefined' ? 'Title is not found':title; //타이틀 미입력시.
+        wrapChecker = wrap.length != 0 ? true:false;
+
+        /* child selected */
+        selected = Jthis.parents('[data-toggle="tooltip"]'); // childe selected check
+        proper   = selected.length == 0 ? Jthis.parent():selected.parent(); // 1 = child selected
+        wrap     = wrapChecker ? wrap:proper; 
+
+
+        /* title 체크 */
+        let Attrcheck = Jthis.data('toggle');
+        Jthis = typeof Attrcheck == 'undefined' ? Jthis.parents('[data-toggle="tooltip"]'):Jthis;
+        seting.title = typeof Jthis.attr('title') == 'undefined' ? seting.title:Jthis.attr('title'); //타이틀 미입력시.
     }
 
-    function SetPosition(data){
+    function tooltipSize(){
         let TW = tooltip.find('.title').width();          // 보여지는 크기
-                 tooltip.find('.title').css('width',TW);
-            TW = tooltip.find('.title').outerWidth(true); // 실제 크기를 할당해 left 값을 계산한다.
-        let TH = tooltip.find('.title').outerHeight(true);
-        data['TWidth']  = TW;
-        data['THeight'] = TH;
+        tooltip.find('.title').css('width',TW);
 
-        let setData = underLine(data);
+        seting.tooltip.width  = tooltip.find('.title').outerWidth(true);
+        seting.tooltip.height = tooltip.find('.title').outerHeight(true);
+    }
 
-        tooltip.offset({top:setData.box_top});
-        tooltip.offset({left:setData.box_left});
+    // function tolltip_Line(data, line='under'){
+    //     tooltip.addClass(line+'Line');
+    // }
+
+    function SetPosition(){
+        tooltipSize();
+        underLine();
+
+        tooltip.addClass(seting.position);
+        tooltip.offset({top:seting.tooltip.top});
+        tooltip.offset({left:seting.tooltip.left});
         tooltip.css('right','auto');
         tooltip.css('bottom','auto');
-        tooltip.find('.title').offset({left:setData.tip_left});
-        tooltip.css('opacity',1);   
+        tooltip.find('.title').offset({left:seting.body.left});
+        tooltip.css('opacity',1);
     }
 
+    function underLine(){
+        let left = wrapChecker ? seting.selecter.left:seting.selecter.left - seting.parent.left;
 
-    function underLine(data){
-        let horizontal  = (data['TWidth']-data['width'])/2;    // Tooltip Width - Selecter Width / 2; ( 가로 중앙 정렬 )
-        let offset_mid  = data['Left']-horizontal;             // center left 값.
-        let width       = data['TWidth']+offset_mid;
-        let max_width   = data['PWidth'];
-
+        /* LEFT horizontal center && wraper inlne */
+        let horizontal  = (seting.tooltip.width - seting.selecter.width)/2;    // Tooltip Width - Selecter Width / 2; ( 가로 중앙 정렬 )
+        let width_align = left - horizontal;                   // center left 값.
+        let width       = seting.tooltip.width + width_align;
+        let max_width   = seting.parent.width;
         let overSize    = width > max_width ? width - max_width:0;
-        let inline      = ((overSize+10)*(-1)) + offset_mid;
-        let TLeft       = offset_mid <= 0 ? 10:overSize > 0 ? inline:'auto'; // 컨텐츠를 벗어날 경우 내부로. 
+        let inline      = ((overSize)*(-1)) + width_align;
+        let TLeft       = width_align < 0 ? 0:overSize > 0 ? inline:'auto'; // 컨텐츠를 벗어날 경우 내부로. 
 
-        /* under line */
-        let top      = data['Top'] + data['height'];
-        let box_top  = wrapChecker ? top + data['PTop']:top; 
-        let box_left = offset_mid + data['PLeft'];
-        let tip_left = TLeft + data['PLeft'];
+        seting.tooltip.left  = wrapChecker ? width_align + seting.parent.left:seting.selecter.left-horizontal;
+        seting.body.left     = TLeft + seting.parent.left;
 
-        return {
-            box_top :box_top,
-            box_left:box_left,
-            tip_left:tip_left
-        }
-    }
+        /* under top set */
+        let under_top = seting.parent.top + seting.selecter.top + seting.selecter.height;  
+        /* over top set */
+        let over_top  = seting.parent.top + seting.selecter.top - seting.tooltip.height; 
+        let max_top   = seting.parent.top + seting.parent.height;
+        let top       = seting.parent.top + under_top + seting.tooltip.height;
 
-    function overLine(data){
-
+        /* under || over check */
+        if(max_top < top){
+            seting.position    = 'over';
+            seting.tooltip.top = over_top;        
+            /* all out check */
+            if(seting.tooltip.top < wrap.offset().top){
+               seting.position    = 'under';
+               seting.tooltip.top = under_top;
+            }         
+        } else {
+            seting.position    = 'under';
+            seting.tooltip.top = under_top;
+        }  
     }
 
     function rightLine(data){
-
-         let vertical    = (data['THeight'] - data['height'])/2;  // Tooltip Width - Selecter Width / 2; ( 세로 중앙 정렬 ) 
-         let mid_height  = data['Top'] - vertical;                // center top 값. 
-
+         let vertical    = (data['theight'] - data['height'])/2;  // Tooltip Width - Selecter Width / 2; ( 세로 중앙 정렬 ) 
+         let mid_height  = data['top'] - vertical;                // center top 값. 
     }
 
 }
